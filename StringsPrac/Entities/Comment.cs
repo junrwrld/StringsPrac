89c444738ae49c01415a4770eafa1073e3866855
João Junior <batistasousajoao@gmail.com>
2023-12-13 18:25:01 -0300

    Add project files.

---
+++ StringsPrac/Entities/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsPrac.Entities
+{
+     class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+         
+        }
+
+        public Comment(String text)
+        {
+            Text = text;
+        }
+
+
+
+    }
+}
